c8d817af57bf148dcd162c55c9ad7f329e87f180
D1rol2010 <d1rol2010@yandex.ru>
2018-12-20 16:05:52 +0200

    'progect'

---
+++ start_dev/gulpfile.js
@@ -0,0 +1,107 @@
+var gulp           = require('gulp'),
+		sass           = require('gulp-sass'),
+		browsersync    = require('browser-sync'),
+		concat         = require('gulp-concat'),
+		uglify         = require('gulp-uglify'),
+		cleancss       = require('gulp-clean-css'),
+		rename         = require('gulp-rename'),
+		autoprefixer   = require('gulp-autoprefixer'),
+		notify         = require("gulp-notify"),
+		sourcemaps 		 = require('gulp-sourcemaps'),
+		// Pug
+		plumber 			 = require('gulp-plumber'),
+
+
+		pug 					 = require('gulp-pug');
+
+
+// Libs concat & minify
+gulp.task('js_libs', function() {
+	return gulp.src([
+		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/jquery.inputmask/jquery.inputmask.bundle.min.js',
+		'app/libs/magnific-popup/jquery.magnific-popup.min.js',
+		// 'app/libs/owl.carousel/owl.carousel.js',
+		// 'app/libs/fancybox3/jquery.fancybox.min.js',
+		// 'app/libs/fullpage.js/fullpage.js',
+		// 'app/libs/Modernizr/modernizr.js',
+		// 'app/libs/jquery-ui/jquery-ui.min.js',
+		// 'app/libs/jQueryFormStyler/jquery.formstyler.min.js',
+		// 'app/libs/jScroll_Pane/jquery.mousewheel.js',
+		// 'app/libs/jScroll_Pane/jquery.jscrollpane.min.js',
+		// add plugins
+		])
+	.pipe(concat('libs.min.js'))
+	.pipe(uglify()) // Mifify js (opt.)
+	.pipe(gulp.dest('app/js'))
+	.pipe(browsersync.reload({ stream: true }))
+});
+
+// Browser sync
+gulp.task('browser-sync', function() {
+	browsersync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false,
+		open: true,
+		// tunnel: true,
+		// tunnel: "projectmane", //Demonstration page: http://projectmane.localtunnel.me
+	})
+});
+
+// main sass
+gulp.task('sass', function() {
+	return gulp.src('app/sass/[^_]*.sass')
+	.pipe(sourcemaps.init())
+	.pipe(sass({ outputStyle: 'expand' }).on('error', notify.onError()))
+	.pipe(rename('main.min.css'))
+	.pipe(autoprefixer(['last 5 versions']))
+	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
+	.pipe(sourcemaps.write('.'))
+	.pipe(gulp.dest('app/css'))
+	.pipe(browsersync.reload( {stream: true} ))
+});
+
+
+gulp.task('scss', function () {
+	var postcss = require('gulp-postcss');
+    var tailwindcss = require('tailwindcss');
+
+    return gulp.src('app/sasss/app.scss')
+        .pipe(sass())
+        .pipe(postcss([tailwindcss('./tailwind.js')]))
+        .pipe(gulp.dest('app/css'))
+        .pipe(browsersync.reload( {stream: true} ))
+});
+
+// Libs sass
+gulp.task('sass_libs', function() {
+	return gulp.src('app/sass/libs.sass')
+	.pipe(sass({ outputStyle: 'expand' }).on('error', notify.onError()))
+	.pipe(rename('libs.min.css'))
+	.pipe(autoprefixer(['last 5 versions']))
+	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
+	.pipe(gulp.dest('app/css'))
+	.pipe(browsersync.reload( {stream: true} ))
+});
+
+gulp.task('pug', function() {
+	return gulp.src('app/pug/pages/*.pug')
+		.pipe(plumber())
+		.pipe(pug({pretty: true}))
+		.pipe(gulp.dest('app/'))
+		.pipe(browsersync.stream());
+});
+
+// Watch tasks
+gulp.task('watch', ['sass', 'scss', 'sass_libs', 'js_libs', 'pug', 'browser-sync'], function() {
+	gulp.watch(['app/sass/**/*.sass','!app/sass/libs/libs.sass'], ['sass']);
+	gulp.watch('app/sasss/**/*.scss', ['scss']);
+	gulp.watch('gulpfile.js', ['js_libs']);
+	gulp.watch('app/sass/libs.sass', ['sass_libs']);
+	gulp.watch('app/js/main.js', browsersync.reload);
+	gulp.watch('app/pug/**/*.pug', ['pug']);
+});
+
+gulp.task('default', ['watch']);
---
+++ start_dev/readme.md
@@ -0,0 +1,20 @@
+# start_dev
+#### Front-end developing start template
+
+Author: [hattam](https://hattam.ru)
+
+![alt](https://raw.githubusercontent.com/hattamsoyunov/start_dev/master/app/img/start_dev_preview.jpg)
+
+## How to use
+
+**Need to ensure you have everything on your machine:**
+1. Node.js
+2. Bower (npm i -g bower)
+
+**In root of template, use commands:**
+1. npm i (Install Node Modules)
+2. gulp (Run start_dev)
+
+### Install jQuery libraries through Bower
+1. bower i plugin-name;
+2. Then place all jQuery libraries paths in the 'js_libs' task (gulpfile.js);
